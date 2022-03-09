8e30ecda74add3f5bb098c244996912634a3312f
Nahuel Bulian <nbulian@gmail.com>
2022-03-09 09:39:11 -0300

    Adding a layout using semantic

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+
+const Header = (props) => {
+    return (
+        <Menu>
+            <Menu.Item>
+                MyIcon
+            </Menu.Item>
+
+            <Menu.Menu position='right'>
+                <Menu.Item>
+                    Campaigns
+                </Menu.Item>
+                <Menu.Item>
+                    +
+                </Menu.Item>
+            </Menu.Menu>
+
+        </Menu>
+    )
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import factory from '../ethereum/factory'
+import { Button, Card } from 'semantic-ui-react';
+import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -8,8 +10,36 @@ class CampaignIndex extends Component {
         return { campaigns };
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View</a>,
+                fluid: true
+            }
+        });
+
+        return <Card.Group items={items} />;
+    }
+
     render() {
-        return <div>{this.props.campaigns[0]}</div>;
+        return (
+            <Layout>
+                <div>
+                    <link
+                        async
+                        rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+                    />
+                    <h3>Open Campaigns</h3>
+                    {this.renderCampaigns()}
+                    <Button
+                        content="Create Campaign"
+                        icon="add circle"
+                        primary // equal primary={true}
+                    />
+                </div>
+            </Layout>);
     }
 }
 
